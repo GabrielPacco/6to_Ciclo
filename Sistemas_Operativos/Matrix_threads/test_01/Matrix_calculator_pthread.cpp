40feadf8fd713fbe05fd72f1fef0ddb64d5133cd
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-11-20 21:05:33 -0500

    Culminacion de las operaciones con hilos

--- Sistemas_Operativos/Matrix_threads/test_01/Matrix_calculator_pthread.cpp
+++ Sistemas_Operativos/Matrix_threads/test_01/Matrix_calculator_pthread.cpp
@@ -70,6 +70,7 @@ void printMatrix(int** matrix, int m, int n){
 	}
 }
 
+// Multiplica dos matrices de tamaño n x n
 void* MatrixCalculator (void * ob){
 
 	Matrix* ob1 = new Matrix;
@@ -114,13 +115,15 @@ void* MatrixCalculator (void * ob){
 	pthread_exit((void*)ob1);
 }
 
-// Suma de matrices usando hilos
-void *Sum(void *ob){
-
+// Función que realiza la suma de 2 matrices usando hilos
+// Recibe el tamaño de las matrices
+// Devuelve la matriz resultante de la suma
+void* Sum_matrix (void * ob)
+{
 	Matrix* ob1 = new Matrix;
 	ob1 = (Matrix*) ob;
 
-	int noOfElements = ob1->firstMatrixM*ob1->secondMatrixN;
+	int noOfElements = ob1->firstMatrixM*ob1->firstMatrixN;
 	int noOfOp = noOfElements/ob1->noOfProcesses;
 	int remaining = noOfElements % ob1->noOfProcesses;
 
@@ -147,25 +150,21 @@ void *Sum(void *ob){
 		const int row=i%ob1->firstMatrixM;
 		const int col=i/ob1->firstMatrixM;
 
-		float result1=0;
-
-		for(int j=0;j<ob1->firstMatrixM;j++)
-		{
-			result1 += ob1->firstMatrix[row][j]+ob1->secondMatrix[j][col];
-		}
-
-		ob1->result[row][col]=result1;
+		ob1->result[row][col]=ob1->firstMatrix[row][col]+ob1->secondMatrix[row][col];
 	}
 	pthread_exit((void*)ob1);
 }
 
-// Multiplicación de matrices usando hilos
-void *Multiply(void *ob){
+// Función que realiza la resta de 2 matrices usando hilos
+// Recibe el tamaño de las matrices
+// Devuelve la matriz resultante de la resta
 
+void* Sub_matrix (void * ob)
+{
 	Matrix* ob1 = new Matrix;
 	ob1 = (Matrix*) ob;
 
-	int noOfElements = ob1->firstMatrixM*ob1->secondMatrixN;
+	int noOfElements = ob1->firstMatrixM*ob1->firstMatrixN;
 	int noOfOp = noOfElements/ob1->noOfProcesses;
 	int remaining = noOfElements % ob1->noOfProcesses;
 
@@ -192,66 +191,12 @@ void *Multiply(void *ob){
 		const int row=i%ob1->firstMatrixM;
 		const int col=i/ob1->firstMatrixM;
 
-		float result1=0;
-
-		for(int j=0;j<ob1->firstMatrixM;j++)
-		{
-			result1 += ob1->firstMatrix[row][j]*ob1->secondMatrix[j][col];
-		}
-
-		ob1->result[row][col]=result1;
+		ob1->result[row][col]=ob1->firstMatrix[row][col]-ob1->secondMatrix[row][col];
 	}
 	pthread_exit((void*)ob1);
 }
 
-// Resta de matrices usando hilos
-void *Subtract(void *ob){
-
-	Matrix* ob1 = new Matrix;
-	ob1 = (Matrix*) ob;
-
-	int noOfElements = ob1->firstMatrixM*ob1->secondMatrixN;
-	int noOfOp = noOfElements/ob1->noOfProcesses;
-	int remaining = noOfElements % ob1->noOfProcesses;
-
-	int firstOperation,lastOperation;
-
-	sem_wait(&semaphore1);
-	if(ob1->threadNumber[ob1->index] == 0){
-
-		firstOperation = noOfOp*ob1->threadNumber[0];
-		lastOperation = noOfOp*(ob1->threadNumber[0]+1)+remaining;
-
-		ob1->index++;
-	}
-	else
-	{
-		firstOperation = noOfOp*ob1->threadNumber[ob1->index]+remaining;
-		lastOperation = noOfOp*(ob1->threadNumber[ob1->index]+1)+remaining;
-		ob1->index++;
-	} 
-
-	sem_post(&semaphore1);
-
-	for(int i =firstOperation;i<lastOperation;i++){
-		const int row=i%ob1->firstMatrixM;
-		const int col=i/ob1->firstMatrixM;
-
-		float result1=0;
-
-		for(int j=0;j<ob1->firstMatrixM;j++)
-		{
-			result1 += ob1->firstMatrix[row][j]-ob1->secondMatrix[j][col];
-		}
-
-		ob1->result[row][col]=result1;
-	}
-	pthread_exit((void*)ob1);
-}
-
-
-
-// Funcion para multiplicar matrices usando hilos
+// Medir el tiempo para las 3 operaciones
 void test(int** firstMatrix, int** secondMatrix, int** result, int firstMatrixM, int firstMatrixN, int secondMatrixM, int secondMatrixN, int noOfProcesses){
 
 	Timer timer;
@@ -286,6 +231,72 @@ void test(int** firstMatrix, int** secondMatrix, int** result, int firstMatrixM,
 
 	pthread_attr_destroy(&attr);
 	sem_destroy(&semaphore1);
+
+	// Suma de matrices
+	Matrix *ob1 = new Matrix;
+	ob1->firstMatrix = firstMatrix;
+	ob1->secondMatrix = secondMatrix;
+	ob1->result = result;
+	ob1->firstMatrixM = firstMatrixM;
+	ob1->firstMatrixN = firstMatrixN;
+	ob1->secondMatrixM = secondMatrixM;
+	ob1->secondMatrixN = secondMatrixN;
+	ob1->noOfProcesses = noOfProcesses;
+
+	pthread_t threads1[noOfProcesses];
+	pthread_attr_t attr1;
+	pthread_attr_init(&attr1);
+	pthread_attr_setdetachstate(&attr1, PTHREAD_CREATE_JOINABLE);
+
+	sem_init(&semaphore1,0,1);
+
+	for(int i = 0; i < noOfProcesses; i++){
+		ob1->threadNumber[i] = i;
+	}
+
+	for(int i = 0; i < noOfProcesses; i++){
+		pthread_create(&threads1[i], &attr1, Sum_matrix, (void*)ob1);
+	}
+
+	for(int i = 0; i < noOfProcesses; i++){
+		pthread_join(threads1[i], NULL);
+	}
+
+	pthread_attr_destroy(&attr1);
+	sem_destroy(&semaphore1);
+
+	// Resta de matrices
+	Matrix *ob2 = new Matrix;
+	ob2->firstMatrix = firstMatrix;
+	ob2->secondMatrix = secondMatrix;
+	ob2->result = result;
+	ob2->firstMatrixM = firstMatrixM;
+	ob2->firstMatrixN = firstMatrixN;
+	ob2->secondMatrixM = secondMatrixM;
+	ob2->secondMatrixN = secondMatrixN;
+	ob2->noOfProcesses = noOfProcesses;
+
+	pthread_t threads2[noOfProcesses];
+	pthread_attr_t attr2;
+	pthread_attr_init(&attr2);
+	pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_JOINABLE);
+
+	sem_init(&semaphore1,0,1);
+
+	for(int i = 0; i < noOfProcesses; i++){
+		ob2->threadNumber[i] = i;
+	}
+
+	for(int i = 0; i < noOfProcesses; i++){
+		pthread_create(&threads2[i], &attr2, Sub_matrix, (void*)ob2);
+	}
+
+	for(int i = 0; i < noOfProcesses; i++){
+		pthread_join(threads2[i], NULL);
+	}
+
+	pthread_attr_destroy(&attr2);
+	sem_destroy(&semaphore1);
 }
 
 int main(int argc, char* argv[]){
@@ -309,7 +320,7 @@ int main(int argc, char* argv[]){
 	fillMatrix(firstMatrix, t, t);
 	fillMatrix(secondMatrix, t, t);
 
-	// Multiplicar matrices
+	// Testear el tiempo para las 3 operaciones
 	test(firstMatrix, secondMatrix, result, t, t, t, t, noOfProcesses);
 
 	/*
